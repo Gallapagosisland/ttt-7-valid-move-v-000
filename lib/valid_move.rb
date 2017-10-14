9cffb0d27369d2f4a9412ea4c83a52efb196e130
Connor Gallopo <connor.gallopo@me.com>
2017-10-14 14:27:38 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,9 @@ def valid_move?(board, index)
     return false
   elsif index > 9 || index < 1
     return false
-  elsif index.between?(1,9)
+  elsif index.between?(1,9) && !position_taken?(board, index)
+    return false
+  elsif index.between?(1,9) 
     return true
 
   end
